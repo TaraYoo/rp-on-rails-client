03779d8491c9cfe170a6bc8d69a15bb1e2128e0f
TaraYoo <yootar@gmail.com>
2019-03-13 19:24:38 -0400

    Add additional classes to bokbulbok cards
    Add additional classes to bokbulbok template

--- assets/scripts/bokbulboks/events.js
+++ assets/scripts/bokbulboks/events.js
@@ -51,7 +51,6 @@ const addHandlers = () => {
   $('.nav-wrapper').on('click', '#get-bokbulboks-button', onGetBokbulboks)
   $('.nav-wrapper').on('click', '#add-bokbulbok-button', onAddBokbulbokPressed)
   $('.authorized-form').on('submit', '#create-bokbulbok-form', onAddBokbulbok)
-  $('.location-cards').on('click', '#get-bokbulbok', getRandomBokbulbok)
   $('.authorized-form').on('click', '.cancel-btn', cancelForms)
 }
 
